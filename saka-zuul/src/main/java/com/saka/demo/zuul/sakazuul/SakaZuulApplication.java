c49ab0b056a50966889ac06e081e2d673995c3cf
sakamoto <fabiojapa@gmail.com>
2019-02-13 22:10:30 -0200

    retirada de client

--- saka-zuul/src/main/java/com/saka/demo/zuul/sakazuul/SakaZuulApplication.java
+++ saka-zuul/src/main/java/com/saka/demo/zuul/sakazuul/SakaZuulApplication.java
@@ -2,11 +2,9 @@ package com.saka.demo.zuul.sakazuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableDiscoveryClient
 @SpringBootApplication
 public class SakaZuulApplication {
 
