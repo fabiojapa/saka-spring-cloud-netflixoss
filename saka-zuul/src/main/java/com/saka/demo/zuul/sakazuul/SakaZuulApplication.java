473c2caa0b60a6a136b455737b71297de9b69744
sakamoto <fabiojapa@gmail.com>
2019-02-13 22:05:02 -0200

    first commit

---
+++ saka-zuul/src/main/java/com/saka/demo/zuul/sakazuul/SakaZuulApplication.java
@@ -0,0 +1,18 @@
+package com.saka.demo.zuul.sakazuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SakaZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakaZuulApplication.class, args);
+	}
+
+}
+
---
+++ saka-zuul/src/test/java/com/saka/demo/zuul/sakazuul/SakaZuulApplicationTests.java
@@ -0,0 +1,17 @@
+package com.saka.demo.zuul.sakazuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SakaZuulApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
